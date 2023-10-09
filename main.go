523b8699a84b54f8bc9a6478ef4cd96cadb97d17
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-10-08 21:08:37 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]string)
+	data["message"] = "Hello!"
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(data)
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	data := make(map[string]string)
+	err := decoder.Decode(&data)
+	if err != nil {
+		data["message"] = "error"
+	}
+	json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8000"
+	}
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/echo", echoHandler)
+	http.ListenAndServe(":"+port, nil)
+}
